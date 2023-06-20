420e14b60189111b172d7bf13405bc582548ddbf
Changyuan Yang <qa894178522@qq.com>
2023-06-20 23:54:39 +0800

    style: eslint

--- src/web.ts
+++ src/web.ts
@@ -3,7 +3,7 @@ import { WebPlugin } from '@capacitor/core';
 import type { NordicDFUPlugin } from './definitions';
 
 export class NordicDFUWeb extends WebPlugin implements NordicDFUPlugin {
-  async startDFU(_options: { fileurl: string, peripheral: string }): Promise<void> {
+  async startDFU(): Promise<void> {
     console.warn('Can not startDFU in Web.');
   }
 }
