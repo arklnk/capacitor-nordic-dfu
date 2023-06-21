8ebf6a86189a5678dfc4e7e13f1c3a719ab2e282
Changyuan Yang <qa894178522@qq.com>
2023-06-21 09:41:22 +0800

    feat: add event type

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,12 @@
+import type { PluginListenerHandle } from '@capacitor/core';
+
 export interface NordicDFUPlugin {
   startDFU(options: { fileurl: string, peripheral: string }): Promise<void>;
+
+  addListener(
+    eventName: 'dfuStateDidChange',
+    listenerFunc: (params: { state: string }) => void,
+  ): Promise<PluginListenerHandle> & PluginListenerHandle;
+
+  removeAllListeners(): Promise<void>;
 }
