1a72a55ef660225eb245788bfb539e12bcb06bee
Changyuan Yang <qa894178522@qq.com>
2023-06-21 17:14:13 +0800

    wip(iOS): update params

--- ios/Plugin/NordicDFUPlugin.swift
+++ ios/Plugin/NordicDFUPlugin.swift
@@ -9,7 +9,8 @@ import iOSDFULibrary
 @objc(NordicDFUPlugin)
 public class NordicDFUPlugin: CAPPlugin, LoggerDelegate, DFUServiceDelegate, DFUProgressDelegate {
 
-    public func dfuProgressDidChange(for part: Int, outOf totalParts: Int, to progress: Int, currentSpeedBytesPerSecond: Double, avgSpeedBytesPerSecond: Double) {
+    public func dfuProgressDidChange(for part: Int, outOf totalParts: Int, to progress: Int,
+                                     currentSpeedBytesPerSecond: Double, avgSpeedBytesPerSecond: Double) {
         
     }
     
@@ -26,18 +27,18 @@ public class NordicDFUPlugin: CAPPlugin, LoggerDelegate, DFUServiceDelegate, DFU
     }
 
     @objc func startDFU(_ call: CAPPluginCall) {
-        guard let fileUrlString = call.getString("fileurl") else {
-            call.reject("fileurl is empty")
+        guard let filePath = call.getString("filePath") else {
+            call.reject("filePath is empty")
             return
         }
         
-        guard let fileURL = URL(string: fileUrlString) else {
-            call.reject("invalid fileurl")
+        guard let fileURL = URL(string: filePath) else {
+            call.reject("invalid filePath")
             return
         }
         
-        guard let peripheral = call.getString("peripheral") else {
-            call.reject("peripheral is empty")
+        guard let deviceAddress = call.getString("deviceAddress") else {
+            call.reject("deviceAddress is empty")
             return
         }
         
@@ -46,16 +47,22 @@ public class NordicDFUPlugin: CAPPlugin, LoggerDelegate, DFUServiceDelegate, DFU
             return
         }
         
-        guard let deviceUUID = UUID(uuidString: peripheral) else {
-            call.reject("invalid peripheral uuid")
+        guard let deviceUUID = UUID(uuidString: deviceAddress) else {
+            call.reject("invalid deviceAddress")
             return
         }
         
+        let forceDfu = call.getBool("forceDfu", false)
+        let enableUnsafeExperimentalButtonlessServiceInSecureDfu = call.getBool("enableUnsafeExperimentalButtonlessServiceInSecureDfu", false)
+        let disableResume = call.getBool("disableResume", false)
+        
         do {
-            let dfuFirmware = try DFUFirmware(urlToZipFile: URL(string: fileUrlString)!)
+            let dfuFirmware = try DFUFirmware(urlToZipFile: fileURL)
             let dfuInitiator = DFUServiceInitiator(queue: DispatchQueue(label: "DFU"))
             
-            dfuInitiator.forceDfu = false
+            dfuInitiator.forceDfu = forceDfu
+            dfuInitiator.enableUnsafeExperimentalButtonlessServiceInSecureDfu = enableUnsafeExperimentalButtonlessServiceInSecureDfu
+            dfuInitiator.disableResume = disableResume
             
             // delegate
             dfuInitiator.logger = self
