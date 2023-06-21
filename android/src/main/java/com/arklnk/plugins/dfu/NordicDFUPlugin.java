d3292c95d0bfa04ef06245e5186993e0ebfdfc05
Changyuan Yang <qa894178522@qq.com>
2023-06-21 10:21:54 +0800

    chore(android): downgrade gradle to v7

--- android/src/main/java/com/arklnk/plugins/dfu/NordicDFUPlugin.java
+++ android/src/main/java/com/arklnk/plugins/dfu/NordicDFUPlugin.java
@@ -1,6 +1,5 @@
 package com.arklnk.plugins.dfu;
 
-import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
@@ -8,15 +7,8 @@ import com.getcapacitor.annotation.CapacitorPlugin;
 
 @CapacitorPlugin(name = "NordicDFU")
 public class NordicDFUPlugin extends Plugin {
-
-    private NordicDFU implementation = new NordicDFU();
-
     @PluginMethod
     public void echo(PluginCall call) {
-        String value = call.getString("value");
 
-        JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
-        call.resolve(ret);
     }
 }
